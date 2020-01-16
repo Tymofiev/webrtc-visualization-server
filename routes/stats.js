4e54836dd175b06aec6486de4aa2cae90be23a11
Tymofiev <ilia.tumofiev@gmail.com>
2020-01-16 20:04:42 +0200

    basic server with one get request connected to local db

--- routes/stats.js
+++ routes/stats.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const { Client } = require('pg')
+const config = require('config')
 
 const router = express.Router()
-
-router.get('/', (req, res) => {
-	res.send({ isWorking: true, text: 'lets get it started' })
+const connectionString = config.get('connectionString')
+const client = new Client({
+	connectionString: connectionString,
 })
 
-router.post('/', (req, res) => {})
+router.get('/', (req, res) => {
+	client
+		.connect()
+		.then(() => {
+			console.log('Connected')
+			client
+				.query('select * from public.statistics;')
+				.then((result) => res.send(result.rows))
+				.catch((e) => console.error(e.stack))
+		})
+		.catch((e) => console.error(e.stack))
+})
 
 router.get('/:id', (req, res) => {})
 
