6da7d4a1ffaadcb4add6e54eca578b4a910a5e54
Tymofiev <ilia.tumofiev@gmail.com>
2020-01-30 12:48:36 +0200

    computing request with filter in a req.query

--- routes/stats.js
+++ routes/stats.js
@@ -1,53 +1,20 @@
 const express = require('express')
-const { Pool } = require('pg')
-const config = require('config')
-
-const StatSerializer = require('../lib/serializer')
 const router = express.Router()
 
-const connectionString = config.get('connectionString')
-
-const pool = new Pool({
-	connectionString: connectionString,
-})
+const { findAll, findById, findByLessonId } = require('../lib/db')
 
 router.get('/', (req, res) => {
-	pool.connect((err, client, done) => {
-		client
-			.query('select * from public.statistics limit 10;')
-			.then((result) => {
-				var stats = StatSerializer.serialize(result.rows)
-				//res.send(stats)
-				res.send(config.get('all-data'))
-				done()
-			})
-			.catch((e) => console.error(e.stack))
+	const { filter } = req.query
 
-		if (err) {
-			return console.error('Error running query', err)
-		}
-	})
+	if (!filter) {
+		findAll(res)
+	} else {
+		findByLessonId(filter.lesson_id, res)
+	}
 })
 
 router.get('/:id', (req, res) => {
-	const { id } = req.params
-	pool.connect((err, client, done) => {
-		client
-			.query(`select * from public.statistics where id = ${id}`)
-			.then((result) => {
-				var stats = StatSerializer.serialize(result.rows)
-				const obj = {
-					data: stats.data[0],
-				}
-				res.send(config.get('single-data'))
-				done()
-			})
-			.catch((e) => console.error(e.stack))
-
-		if (err) {
-			return console.error('Error running query', err)
-		}
-	})
+	findById(req, res)
 })
 
 module.exports = router
